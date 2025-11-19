20d1cde0a1518faebb5fcb080a7769589224e6de
Muhammad Zaki Azhar <149247787+muzazr@users.noreply.github.com>
2025-11-19 15:33:56 +0700

    Change HomeScreen to a StatelessWidget
    Refactor HomeScreen from StatefulWidget to StatelessWidget.

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
